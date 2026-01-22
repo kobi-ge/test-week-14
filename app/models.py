24db2079821b725d906427fcf62e45ee44592126
kobi <kodigershuni@gmail.com>
2026-01-22 10:10:04 +0200

    added Data Processing functions

---
+++ app/models.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv("weapons_list.csv")
+
+def add_risk_level(df):
+    bins = [0, 20, 100, 300, np.inf]
+    lab = ["low", "medium", "high", "extreme"]
+    df['risk_level'] = pd.cut(df['range_km'], bins=bins, labels=lab)
+    return df
+
+def clean_null(df):
+    df = df.fillna("unknown")
+    return df
